c7db5f0e18bc250011773f52f074f96e7baca184
henrcaio <caioh96@gmail.com>
2022-07-15 20:35:58 -0300

    first commit

---
+++ Entities/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_set.Entities {
+    class Student {
+        public int StudentNumber { get; set; }
+
+        public Student(int studentNumber) {
+            StudentNumber = studentNumber;
+        }
+
+        public override int GetHashCode() {
+            return StudentNumber.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if(!(obj is Student)) {
+                return false;
+            }
+            Student other = obj as Student;
+            return StudentNumber.Equals(other.StudentNumber);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Exercise_set.Entities;
+using System.Collections.Generic;
+
+namespace Exercise_set {
+    class Program {
+        static void Main(string[] args) {
+
+            HashSet<Student> set = new HashSet<Student>();
+
+            Console.Write("How many students for course A? ");
+            int courseA = int.Parse(Console.ReadLine());
+            for (int i = 0; i < courseA; i++) {
+               int x = int.Parse(Console.ReadLine());
+                set.Add(new Student(x));
+            }
+
+            Console.Write("How many students for course B? ");
+            int courseB = int.Parse(Console.ReadLine());
+            for (int i = 0; i < courseB; i++) {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(new Student(x));
+            }
+
+            Console.Write("How many students for course C? ");
+            int courseC = int.Parse(Console.ReadLine());
+            for (int i = 0; i < courseC; i++) {
+                int x = int.Parse(Console.ReadLine());
+                set.Add(new Student(x));
+            }
+
+
+            Console.WriteLine("Total students: " + set.Count);
+        }
+    }
+}
